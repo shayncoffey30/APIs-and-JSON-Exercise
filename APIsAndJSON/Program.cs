fd33ea01088d2ac68b8b954f3eb7c4785528c5c8
Shayn Coffey <Shayncoffey30@gmail.com>
2025-12-18 20:52:51 -0500

    completed

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Net.Http;
 using Newtonsoft.Json.Linq;
+using System.IO;
 
-namespace KanyeAndRon
+
+namespace WeatherApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            QuoteGenerator.KanyeQuote();
-            QuoteGenerator.RonQuote();
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please enter your zipcode");
+            var zipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},& units=imperial&appid={APIKey}";
+
+            Console.WriteLine();
+
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
+
+
+
+
         }
     }
 }
---
+++ APIsAndJSON/WeatherMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherApp
+{
+    public class WeatherMap
+    {
+        public static double GetTemp(string apiCall)
+        {
+
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temp= double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+
+        }
+    }
+}
